6c2f0af3318e2840ccef672615760115db5cb78b
Kim O'Dell <kimbo.odell@gmail.com>
2024-04-04 20:57:17 -0400

    Add cohort column name for cohorts.name

--- 4_queries/8_total_cohort_assistance_time.sql
+++ 4_queries/8_total_cohort_assistance_time.sql
@@ -1,6 +1,6 @@
-SELECT cohorts.name, SUM(completed_at - started_at) AS total_duration
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
 FROM assistance_requests
 JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
+JOIN cohorts on cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY total_duration;
